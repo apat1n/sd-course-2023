38a2eeca6f439103ee2ebe6596446020369c7783
Alexey Lopatin <alexey.lopatin@aitudala.kz>
2023-05-27 19:29:46 +0300

    add simple classes

---
+++ src/main/java/org/example/Backpack.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.List;
+
+public class Backpack {
+    private List<Item> items;
+}
---
+++ src/main/java/org/example/Trap.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import net.slashie.util.Pair;
+
+public class Trap {
+    private Integer attackPower;
+    private Pair<Integer, Integer> coordinates;
+}
